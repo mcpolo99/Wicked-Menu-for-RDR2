f151962ce9df0aa2d3a905465931e7e149a7048a
RandomCrocodile <mawi@polosab.com>
2026-01-25 10:53:00 +0100

    123

--- dev/3_builForm.lua
+++ dev/3_builForm.lua
@@ -1,16 +1,23 @@
 
 --[[
 TODO:
-- Add option we can update Z cordinate in game.
-- Add so we can add new values in the properties window.
+--  Add option we can update Z cordinate in game.
+--  Add so we can add new values in the properties window., pretty much like "save pos" and it creates a new entry somewehre.
+--
 
 ]]
+Logger.currentLevel = Logger.Level.DEBUG
+Navigator = {}
+Navigator.selectedItem = nil -- The item to be used for teleport
+Navigator.listLocations = {} -- a cahce for current cycle or list of avaiable teleport locations, used in load_mapdata etc
+Navigator.target = nil --a copy of the teleport target name/control
+Navigator.selectednodeindex = nil
 
 
 
 nameOfPanel = "CEPanel_NavigatorSelection"
 local mainForm = _G.CETrainer.CEPanel_DynamicControls
-
+local _ref_tree_view = nil
 
 
 -- =====================
@@ -26,8 +33,9 @@ end
 local props = nil
 debugpanel = nil
 
-testsender1 = nil -- USed for updating cordinates outside of this script
-testsender2 = nil
+testsender1 = nil -- USed for updating cordinates outside of this script -- DONT REMOVE 
+testsender2 = nil --debbugging
+testsender3 = nil -- debuging
 cacheTree = nil
 -- NodeData = setmetatable({}, { __mode = "k" }) -- weak keys
 NodeData = {} -- global or upvalue
@@ -63,6 +71,15 @@ local function getItemsCount(t)
     end
 end
 
+local function getNodeByData(data)
+    for nodeIdx, d in pairs(NodeData) do
+        if d == data then
+            return _ref_tree_view.Items[nodeIdx] -- get the node by index
+        end
+    end
+    return nil
+end
+
 local function runTeleport(selectedItem)
     Navigator.selecteditem = selectedItem
     --Logger.debug(selectedItem.x,selectedItem.y,selectedItem.z)
@@ -76,6 +93,14 @@ end
 local function test(str)
     print(str)
 end
+local function getPlayerPos()
+
+    local x=readFloat("[coord2]+80")
+    local y=readFloat("[coord2]+84")
+    local z=readFloat("[coord2]+88")
+    return x , y , z
+end
+
 
 local function serializeTable(t, indent, noChildren)
   indent = indent or 0
@@ -125,6 +150,13 @@ local function serializeTable(t, indent, noChildren)
   return s
 end
 
+local _selectedNodeIndex = nil -- or just use selected index as ref?
+local function getSelectedNodeAndData()
+    local node = _ref_tree_view.Selected
+    if not node then return nil, nil end
+    return node, NodeData[node.AbsoluteIndex]
+end
+
 
 -- =====================
 --  helper Build Tree view
@@ -145,7 +177,7 @@ local function addTreeNodes(parentNode, t)
                 --print(item.text)
             end
             node = parentNode.Add(display,parentNode)
-           
+
             NodeData[node.AbsoluteIndex] = item
 
             -- recurse if nested tables exist
@@ -185,14 +217,26 @@ local function addTreeNodes(parentNode, t)
                 node = parentNode.Add(key,parentNode)
             end
             --node.Data = value --does not work
+            --value.nodeindex = node.AbsoluteIndex
             NodeData[node.AbsoluteIndex] = value
-            value.nodeindex = node.AbsoluteIndex
+
 
 
             addTreeNodes(node, value)
         end
     end
 end
+local function rebuildTree(parent)
+    local tree = _ref_tree_view
+    if not tree then return end
+
+    tree.beginUpdate()
+    tree.Items.Clear()
+    NodeData = {}   -- IMPORTANT
+    addTreeNodes(tree, MapData_Array)
+    tree.endUpdate()
+end
+
 
 local function showProperties(panel, data)
     -- TO AVOID MASSIVE DUPLICATES
@@ -228,7 +272,7 @@ local function showPropertyEditor(panel, data)
     local spacing = 24
     local margin = 5
     local lblWidth = 100
-    local editable = (type(data) == "table" and data.x ~= nil and data.y ~= nil)
+    
 
     local _rPanel = createPanel(panel)
     _rPanel.Align = alClient
@@ -236,6 +280,8 @@ local function showPropertyEditor(panel, data)
     local _lPanel = createPanel(panel)
     _lPanel.Align = alLeft
 
+
+    local readOnlyFields = { ["nodeindex"] = true, ["nodeRef"] = true }
 
     for k,v in pairs(data) do
         if type(v) ~= "table" then
@@ -249,6 +295,12 @@ local function showPropertyEditor(panel, data)
             lbl.Align = alTop
             -- lbl.AutoSize = false
             lbl.Constraints.MinHeight =spacing
+
+
+            local editable = (type(data) == "table" and data.x ~= nil and data.y ~= nil)
+            if readOnlyFields[k] then
+                editable = false
+            end
 
 
             -- edit box
@@ -283,7 +335,20 @@ local function showPropertyEditor(panel, data)
                 else
                     -- string or other types: accept anything
                     data[k] = val
+
                 end
+
+                if k == "text" then
+                    local node = getNodeByData(data)
+                    if node then
+                        node.Text = data.text
+                    end
+                end
+
+
+            end
+            edit.OnExit = function(sender)
+
             end
 
             y = y + spacing
@@ -291,15 +356,179 @@ local function showPropertyEditor(panel, data)
     end
 end
 
+local draggingNode = nil
+local function dragAndDrop(parent)
+
+    parent.DragCursor = crDrag
+    parent.AllowDrop = True
+
+    parent.DragMode = dmAutomatic
+    parent.DragKind = dkDrag
+    parent.ReadOnly = true
+    parent.AllowDrop = true
+    -- parent.OnMouseDown = function(sender, button, x, y)
+    --     --this is registerd and working. 
+    --     print("OnMouseDown")
+    --     testsender2 = sender
+    --     if button == mbLeft then
+    --         draggingNode = sender
+    --     end
+    -- end
+
+    parent.OnEndDrag = function(sender, target, x, y)
+        Logger.debug("OnEndDrag")
+        draggingNode = nil
+    end
+    -- OnEndDrag
+    -- OnDragDrop
+    -- OnDragOver
+    -- OnStartDrag
+
+    parent.OnDragOver = function(sender, source, x, y, state, accept)
+        Logger.debug("OnDragOver", x, y)
+        accept = true
+    end
+    parent.OnDragDrop = function(sender, source, x, y)
+        Logger.debug("OnDragDrop fired")
+        draggingNode = nil
+        -- local target = sender.GetNodeAt(x, y)
+        -- local dragged = sender.Selected
+
+        -- if not target or not dragged then return end
+        -- if dragged.Parent ~= target.Parent then return end
+
+        -- local parentNode = dragged.Parent
+        -- local parentData = NodeData[parentNode.AbsoluteIndex]
+
+        -- if not parentData or not parentData.children then return end
+
+        -- local list = parentData.items
+
+        -- -- find indices
+        -- local fromIdx, toIdx
+        -- for i, v in ipairs(list) do
+        --     if NodeData[dragged.AbsoluteIndex] == v then
+        --         fromIdx = i
+        --     end
+        --     if NodeData[target.AbsoluteIndex] == v then
+        --         toIdx = i
+        --     end
+        -- end
+
+        -- if not fromIdx or not toIdx or fromIdx == toIdx then return end
+
+        -- -- move in data
+        -- local item = table.remove(list, fromIdx)
+        -- table.insert(list, toIdx, item)
+
+        -- -- move visually
+        -- dragged.MoveTo(target, naInsert)
+    end
+
+end
+
+
+local function tree_view_popup(parent)
+
+    local popup_01 = createPopupMenu(parent)
+    parent.PopupMenu = popup_01
+    testsender3 = popup_01
+
+    popup_01.OnPopup = function()
+        local node, data = getSelectedNodeAndData()
+        local allow = ( node ~= nil and type(data) == "table" and data.custom == true )
+
+    end
+    parent.OnMouseDown = function(sender, button, x, y)
+        if button == mbRight then
+            local node, data = getSelectedNodeAndData()
+            local allow = ( node ~= nil and type(data) == "table" and data.custom == true )
+            popup_01.AutoPopup = allow
+
+        end
+    end
+    
+
+
+    local _add_new_item = createMenuItem(popup_01)
+    -- “Add location here” (uses current player coords) ?? 
+    _add_new_item.Caption = "Add location"
+    
+
+    _add_new_item.OnClick = function()
+        Logger.debug("_add_new_item")
+        local node, data = getSelectedNodeAndData()
+        if not node or type(data) ~= "table" then return end
+        if data.custom ~= true then return end
+        if not data.locations then return end
+
+        local _x, _y, _z = getPlayerPos()
+
+        -- create new item
+        local newItem = { text = "new pos",  x = _x,     y=_y , z = _z}
+        table.insert(data.locations, newItem)
+
+        -- visual node
+        local newNode = node.Add(newItem.text, node) -- Create the vvisual node 
+        NodeData[newNode.AbsoluteIndex] = newItem -- store the node ref 
+        node.Expanded = true
+        parent.Selected = newNode
+    end
+    
+
+    local _add_new_group = createMenuItem(popup_01)
+    _add_new_group.Caption = "Add New Group"
+    _add_new_group.OnClick = function()
+        Logger.debug("_add_new_group")
+        local node, data = getSelectedNodeAndData()
+
+        data.locations = data.locations or {}
+
+
+        -- create new item
+        local newItem = { text = "newGroupName", locations = {} , custom = true}
+
+        table.insert(data.locations, newItem) -- insert to MapData_Array
+
+        -- visual node
+        local newNode = node.Add(newItem.text, node) -- Create the node 
+        NodeData[newNode.AbsoluteIndex] = newItem -- store the node ref 
+
+        node.Expanded = true
+        parent.Selected = newNode
+
+    end
+
+
+
+    local _delete_item = createMenuItem(popup_01)
+    _delete_item.Caption = "Delete item"
+    
+    _delete_item.OnClick = function()
+            Logger.debug("_delete_item")
+
+    end
+
+    popup_01.Items.add(_add_new_item)
+    popup_01.Items.add(_delete_item)
+    popup_01.Items.add(_add_new_group)
+
+end
+
+
+
+
 -- =====================
 -- Build the Tree view
 -- =====================
 local function build_tree_view(parent , array)
         -- tree_01.Name =
     local tree_01 = createTreeView(parent)
+    _ref_tree_view = tree_01 -- add for referense elsewhere where i need it in some way 
     tree_01.Align = alClient
     tree_01.ReadOnly = true -- not to modify texts-
-
+    tree_01.DragMode = dmAutomatic
+    tree_01.DragKind = dkDrag
     tree_01.Items.Clear()
     tree_01.ShowButtons=true
     tree_01.RightClickSelect=true
@@ -311,31 +540,13 @@ local function build_tree_view(parent , array)
     end
     tree_01.OnCollapsing = function(sender, node)  -- sends 2 args
     end
+
+    -- TELEPORT ON DUBBLE CLICK ITEM ! 
     tree_01.OnDblClick = function(sender) -- sends 1 arg
-    end
-    tree_01.OnNodeChange = function(sender, node, allowChange) -- no event
-    end
-   tree_01.OnSelectionChanged = function(sender)
         local node = sender.Selected
         if not node then return end
-
         local data = NodeData[node.AbsoluteIndex]
         if not data then return end
-
-        -- Always update property panel
-        showPropertyEditor(props, data)
-
-        Logger.debug("numitems:", getItemsCount(data))
-
-        ----------------------------------------------------------------
-        -- CASE 1: Item has child locations → list mode ONLY
-        ----------------------------------------------------------------
-        if type(data.locations) == "table" then
-            Logger.debug("Setting listLocations",getItemsCount(data.locations))
-            Navigator.listLocations = data.locations
-            Navigator.selecteditem = nil
-            return
-        end
 
         ----------------------------------------------------------------
         -- CASE 2: Single item but missing coordinates → NO teleport
@@ -361,12 +572,46 @@ local function build_tree_view(parent , array)
         )
 
         Navigator.selecteditem = data
+        Navigator.selectednodeindex = node.AbsoluteIndex
 
         local target = "_navigator1_"
         local mr = getAddressList().GetMemoryRecordByDescription(target)
         if mr then
             mr.Active = true
         end
+
+
+    end
+    tree_01.OnNodeChange = function(sender, node, allowChange) -- no event
+    end
+
+    -- FOR NAVIGATOR ROTATION 
+    tree_01.OnSelectionChanged = function(sender)
+        local node = sender.Selected
+        if not node then return end
+
+        local data = NodeData[node.AbsoluteIndex]
+        if not data then return end
+
+        -- Always update property panel
+        showPropertyEditor(props, data)
+
+        Logger.debug("numitems:", getItemsCount(data))
+
+        --print(GF.serializeTable(data,2,true))
+
+        ----------------------------------------------------------------
+        -- CASE 1: Item has child locations → list mode ONLY
+        ----------------------------------------------------------------
+        Navigator.selectednodeindex = node.AbsoluteIndex
+        if type(data.locations) == "table" then
+            Navigator.selectednodeindex = node.AbsoluteIndex
+            Logger.debug("Setting listLocations",getItemsCount(data.locations))
+            Navigator.listLocations = data.locations
+            Navigator.selecteditem = nil
+            return
+        end
+
     end
     tree_01.OnExpanding = function(sender, node) -- sends 2 arg
     end
@@ -374,40 +619,14 @@ local function build_tree_view(parent , array)
     tree_01.beginUpdate()
     addTreeNodes(tree_01, array)
     tree_01.endUpdate()
-
-
-
-    -- local function run(str)
-    --   print(str)
-    --   local selNode = tree_01.Selected
-    --   if not selNode then return end
-
-    --   local item = selNode.Data
-
-
-
-    --   if type(item) == "table" then
-
-    --       for k,v in pairs(item) do
-    --           print(k)
-
-    --       end
-
-    --       -- If it has x,y or x,y,z, call runTeleport
-    --       if item.x and item.y then
-    --           --runTeleport(item)
-    --       end
-
-    --       -- If it’s a group with multiple locations, set listLocations
-    --       if isNumArray(item) and item[1] and (item[1].x or item[1].locations) then
-    --           --Navigator.listLocations = item
-    --       end
-    --   end
-
-    -- end
-    testsender1 = tree_01
+    tree_view_popup(tree_01)
+    dragAndDrop(tree_01)
+    testsender1 = tree_01 -- referense for outside access.
 
 end
+
+
+
 
 -- =====================
 -- building the form
@@ -440,7 +659,7 @@ local function buildForm(form, name ,dataArray)
     topPanel.Align = alTop
     topPanel.top = 1
     topPanel.parent = panel_01
-    topPanel.Height = 200
+    topPanel.Height = 450
 
 
     local splitter = createSplitter(panel_01)
@@ -476,6 +695,9 @@ end
 -- print(panel.Align)
 -- print(panel.Anchors)
 buildForm(mainForm, nameOfPanel,MapData_Array)
+
+
+
 
 
 
--- dev/notes.md
+++ dev/notes.md
@@ -1,4 +1,88 @@
 
+
+# build CE 7.5 for speed hack to work 
+
+
+
+
+install all required packages. 
+
+Download Lazarus 2.2.2 from https://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2064%20bits/Lazarus%202.2.2/ 
+First install lazarus-2.2.2-fpc-3.2.2-win64.exe and then lazarus-2.2.2-fpc-3.2.2-cross-i386-win32-win64.exe
+
+build tools 2013 https://www.microsoft.com/en-us/download/details.aspx?id=40760
+https://community.chocolatey.org/packages?q=build%20tools%20for%20Visual%20Studio%202013
+choco install vcbuildtools
+
+build tools 2017 https://stackoverflow.com/questions/57795314/are-visual-studio-2017-build-tools-still-available-for-download
+install with additional support for  v141_xp c ++
+
+
+sdk 8.1 https://learn.microsoft.com/en-us/windows/apps/windows-sdk/downloads-archive
+Windows 10 SDK, version 1809 (10.0.17763.0) https://learn.microsoft.com/en-us/windows/apps/windows-sdk/downloads-archive
+
+
+
+require 2013 tool set 
+msbuild "Cheat Engine/Direct x mess/Direct x mess.sln" /p:Configuration=Release /p:Platform=Win32
+msbuild "Cheat Engine/Direct x mess/Direct x mess.sln" /p:Configuration=Release /p:Platform=x64
+
+C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VC\VCTargets\Platforms\x64\PlatformToolsets\v141_xp
+msbuild "Cheat Engine/MonoDataCollector/MonoDataCollector.sln" /p:Configuration=Release /p:Platform=Win32
+msbuild "Cheat Engine/MonoDataCollector/MonoDataCollector.sln" /p:Configuration=Release /p:Platform=x64
+
+
+C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\
+msbuild "Cheat Engine/DotNetDataCollector/DotNetDataCollector.sln" /p:Configuration=Release /p:Platform=Win32
+msbuild "Cheat Engine/DotNetDataCollector/DotNetDataCollector.sln" /p:Configuration=Release /p:Platform=x64
+
+
+download nuget.exe https://www.nuget.org/downloads
+ .\nuget.exe restore "Cheat Engine/DotNetInvasiveDataCollector/DotNetInvasiveDataCollector.sln"
+
+msbuild "Cheat Engine/DotNetInvasiveDataCollector/DotNetInvasiveDataCollector.sln" /p:Configuration=Release
+
+
+
+
+need something with java did not care. 
+msbuild "Cheat Engine/Java/CEJVMTI/CEJVMTI.sln" /p:Configuration=Release /p:Platform=Win32
+msbuild "Cheat Engine/Java/CEJVMTI/CEJVMTI.sln" /p:Configuration=Release /p:Platform=x64
+
+i think this worked atleast . 
+msbuild "Cheat Engine/tcclib/win32/tcc/tcc.sln"  /p:Configuration="Output to 64 (Release)" /p:Platform=Win32
+msbuild "Cheat Engine/tcclib/win32/tcc/tcc.sln"  /p:Configuration="Output to 32 (Release)" /p:Platform=Win32
+msbuild "Cheat Engine/tcclib/win32/tcc/tcc.sln"  /p:Configuration="Output to 64 (Release)" /p:Platform=x64
+msbuild "Cheat Engine/tcclib/win32/tcc/tcc.sln"  /p:Configuration="Output to 32 (Release)" /p:Platform=x64
+
+
+all files collected in  Cheat Engine\bin
+
+
+
+https://www.unknowncheats.me/forum/anti-cheat-bypass/504191-undetected-cheat-engine-driver-2022-bypass-anticheats-eac.html
+
+
+
+
+*.ddp  *.pas *.lrt *.lpr *.lpi *.lfm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # converting json list to lua table
 
 ## access to lua table
@@ -8,6 +92,8 @@ examples:
 local p = Path([[G:\Cheats\Tables\Wicked-Menu-for-RDR2\Array]]) /"MapData_Array.lua"
 local p2 = Path([[G:\Cheats\Tables\Wicked-Menu-for-RDR2\]]) /"MapData_Array.lua"
 Path.exportLua(p,p2)
+
+
 
 
 
